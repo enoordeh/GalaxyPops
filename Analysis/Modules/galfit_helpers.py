210d50f09864d3052c019d69a0cf2030da13b5b2
Emil Noordeh <emiln@stanford.edu>
2020-11-09 17:03:29 -0800

    Added visualization tools for COSMOS galfits

--- Analysis/Modules/galfit_helpers.py
+++ Analysis/Modules/galfit_helpers.py
@@ -142,7 +142,7 @@ def run_galfit_parallel(params,fit_df=None,full_df=None,zp=26,
     print('Saving results to {}'.format(save_file))
     new_df.to_csv(save_file,index=False)
     print('Returning results as DataFrame')
-    return new_df
+    return new_df, save_name
 
 
 def run_galfit(row,full_df=None,zp=26,width=90,HLRwidth=False,PSFf=1,
@@ -380,217 +380,221 @@ def run_galfit(row,full_df=None,zp=26,width=90,HLRwidth=False,PSFf=1,
 
     return ID,ra,dec, re, ar, n, m, s, chi2nu, EV
 
-# def showme3(oimg,fignum=None):
-#     vm = np.percentile(oimg[1].data,99)
-# #     print vm
+def showme3(oimg,fignum=None):
+    vm = np.percentile(oimg[1].data,99)
+#     print vm
     
-#     plt.figure(fignum)
-#     plt.subplot(131)
-#     plt.imshow(oimg[1].data,interpolation='none',cmap='viridis',vmin=1e-4,vmax=vm)
-#     plt.subplot(132)
-#     plt.imshow(oimg[2].data,interpolation='none',cmap='viridis',vmin=1e-4,vmax=vm)
-#     plt.subplot(133)
-#     plt.imshow(oimg[3].data,interpolation='none',cmap='viridis',vmin=1e-4,vmax=vm)
+    plt.figure(fignum)
+    plt.subplot(131)
+    plt.imshow(oimg[1].data,interpolation='none',cmap='viridis',vmin=1e-4,vmax=vm)
+    plt.subplot(132)
+    plt.imshow(oimg[2].data,interpolation='none',cmap='viridis',vmin=1e-4,vmax=vm)
+    plt.subplot(133)
+    plt.imshow(oimg[3].data,interpolation='none',cmap='viridis',vmin=1e-4,vmax=vm)
 
-# def plot_by_ID(ID,save_name=None,cos_df=cos_df):
-#     if not save_name:
-#         print 'provide a save_name...'
-#         return
-#     # tdir = '/data/emiln/XLSSU122/analysis/cosmos/galfit_results/'+str(ID)
-#     tdir = '/data/emiln/XLSSC122_GalPops/Data/Products/COSMOS/galfit_results/'+str(ID)
+def plot_by_ID(ID,save_name=None,survey='COSMOS'):
+    if not save_name:
+        print 'provide a save_name...'
+        return
+    tdir = '/data/emiln/XLSSC122_GalPops/Data/Products/'+survey+'/galfit_results/'+str(ID)
     
-# #     ttdf = pd.read_csv('COSMOS_test_galfit_results_'+save_name+'.csv')
-#     results_root = '/data/emiln/XLSSC122_GalPops/Analysis/COSMOS/Results/'
-#     # ttdf = pd.read_csv('results/'+save_name+'.csv')
-#     ttdf = pd.read_csv(results_root+save_name+'.csv')
-#     print 'ID:',ID
-#     print 'ttdf[ID]',ttdf['ID']
-#     print ttdf[ttdf['ID'] == ID]
-# #     print ID
-#     try: 
-#         oimg = fits.open(tdir+'/out_'+save_name+'.fits')
-#         showme3(oimg)
-#     except:
-#         print "No model fit found. Plotting raw image from /cutout.fits"
-#         try:
-#             oimg = fits.open(tdir+'/cutout.fits')
-#             vm = np.percentile(oimg[0].data,99)
-#             plt.figure()
-#             plt.subplot(131)
-#             plt.imshow(oimg[0].data,interpolation='none',cmap='viridis',vmin=1e-4,vmax=vm)
-#         except:
-#             print "No cutout.fits file found..."
-#             oimg = fits.open(tdir+'/data_cps.fits')
-#             vm = np.percentile(oimg[0].data,99)
-#             plt.figure(figsize=(5,5))
-#             plt.subplot()
-#             plt.imshow(oimg[0].data,interpolation='none',cmap='viridis',vmin=1e-4,vmax=vm)
+    results_root = '/data/emiln/XLSSC122_GalPops/Analysis/COSMOS/Results/'
+    ttdf = pd.read_csv(results_root+save_name+'.csv')
+    print ttdf[ttdf['ID'] == ID]
+    try: 
+        oimg = fits.open(tdir+'/out_'+save_name+'.fits')
+        showme3(oimg)
+    except:
+        print "*** No model fit found. Plotting raw image from /cutout.fits ***"
+        try:
+            oimg = fits.open(tdir+'/cutout.fits')
+            vm = np.percentile(oimg[0].data,99)
+            plt.figure()
+            plt.subplot(131)
+            plt.imshow(oimg[0].data,interpolation='none',cmap='viridis',vmin=1e-4,vmax=vm)
+        except:
+            print "*** No cutout.fits file found. Plotting CPS file. ***"
+            oimg = fits.open(tdir+'/data_cps.fits')
+            vm = np.percentile(oimg[0].data,99)
+            plt.figure(figsize=(5,5))
+            plt.subplot()
+            plt.imshow(oimg[0].data,interpolation='none',cmap='viridis',vmin=1e-4,vmax=vm)
         
-#     print '\n ***CATALOG PARAMS***'
-#     print cos_df[cos_df['NUMBER']==ID][['q','n','re']]
-#     plt.show()
+    # print '\n ***CATALOG PARAMS***'
+    # print cos_df[cos_df['NUMBER']==ID][['q','n','re']]
+    plt.show()
+    return
 
-# def compare_df(galfit_file,catalog_df,sky=False):
-#     # Load fits and compare to published catalog
-#     df2 = pd.read_csv(galfit_file)
-#     len(df2[df2['n']==-99])
-#     df = df2[df2['n']!=-99]
-#     cos_df = catalog_df
+def load_COSMOS_published_cat():
+    cosmos_cat_root = '/data/emiln/XLSSC122_GalPops/Data/Products/COSMOS/catalogs/'
+    cos_cat = cosmos_cat_root+'cos_2epoch_wfc3_f125w_060mas_v1.0_galfit.cat'
+    cos_df = pd.read_csv(cos_cat,header=0,delim_whitespace=True,skiprows=[1])
+    cos_df2 = cos_df[cos_df.columns[:-1]]
+    cos_df2.columns = cos_df.columns[1:]
+    return cos_df2
 
-#     plt.hist(cos_df['n'],bins=np.linspace(0,10,20),alpha=0.5,color='blue', label='Catalog')
-#     plt.hist(df['n'],bins=np.linspace(0,10,20), alpha=0.5,color='red', label='Test')
-#     plt.xlabel('Sersic index, n')
-#     plt.legend()
-#     plt.show()
-#     print np.max(cos_df['n'])
+def compare_COSMOS(save_name,catalog_df,sky_test=False):
+    # Load fits and compare to published catalog
+    results_root = '/data/emiln/XLSSC122_GalPops/Analysis/COSMOS/Results/'
+    df2 = pd.read_csv(results_root+save_name+'.csv')
+    # df2 = pd.read_csv(galfit_file)
+    df = df2[df2['n']!=-99]
+    cos_df = catalog_df
 
-#     plt.hist(cos_df['re'],bins=np.linspace(0,6,30),alpha=0.5,color='blue', label='Catalog')
-#     plt.hist(df['re']*0.06,bins=np.linspace(0,6,30), alpha=0.5,color='red', label='Test')
-#     plt.xlabel('Effective radius (\'\')')
-#     plt.legend()
-#     plt.show()
+    plt.hist(cos_df['n'],bins=np.linspace(0,10,20),alpha=0.5,color='blue', label='Catalog')
+    plt.hist(df['n'],bins=np.linspace(0,10,20), alpha=0.5,color='red', label='Test')
+    plt.xlabel('Sersic index, n')
+    plt.legend()
+    plt.show()
+    print np.max(cos_df['n'])
 
-#     plt.hist(cos_df['q'],bins=np.linspace(0,1,20),alpha=0.5,color='blue', label='Catalog')
-#     plt.hist(df['ar'],bins=np.linspace(0,1,20), alpha=0.5,color='red', label='Test')
-#     plt.xlabel('Axis ratio, q')
-#     plt.legend()
-#     plt.show()
+    plt.hist(cos_df['re'],bins=np.linspace(0,6,30),alpha=0.5,color='blue', label='Catalog')
+    plt.hist(df['re']*0.06,bins=np.linspace(0,6,30), alpha=0.5,color='red', label='Test')
+    plt.xlabel('Effective radius (\'\')')
+    plt.legend()
+    plt.show()
+
+    plt.hist(cos_df['q'],bins=np.linspace(0,1,20),alpha=0.5,color='blue', label='Catalog')
+    plt.hist(df['ar'],bins=np.linspace(0,1,20), alpha=0.5,color='red', label='Test')
+    plt.xlabel('Axis ratio, q')
+    plt.legend()
+    plt.show()
     
-#     mdf = df.merge(cos_df,left_on='ID',right_on='NUMBER',suffixes=('_M','_C'))
+    mdf = df.merge(cos_df,left_on='ID',right_on='NUMBER',suffixes=('_M','_C'))
     
-#     print mdf.columns.values
+    # print mdf.columns.values
     
-#     mdf['logn_M'] = np.log10(mdf['n_M'])
-#     mdf['logn_C'] = np.log10(mdf['n_C'])
-#     mdf['re_M'] = mdf['re_M']*0.06
+    mdf['logn_M'] = np.log10(mdf['n_M'])
+    mdf['logn_C'] = np.log10(mdf['n_C'])
+    mdf['re_M'] = mdf['re_M']*0.06
     
-#     mdf['Delta_n'] = mdf['n_M'] - mdf['n_C'] 
-#     mdf['Delta_re'] = mdf['re_M'] - mdf['re_C']
-#     mdf['Delta_q'] = mdf['ar'] - mdf['q']
-#     mdf['Delta_mag'] = mdf['mag_M'] - mdf['mag_C'] 
+    mdf['Delta_n'] = mdf['n_M'] - mdf['n_C'] 
+    mdf['Delta_re'] = mdf['re_M'] - mdf['re_C']
+    mdf['Delta_q'] = mdf['ar'] - mdf['q']
+    mdf['Delta_mag'] = mdf['mag_M'] - mdf['mag_C'] 
     
-#     plt.scatter(mdf['F125W_Kron'],mdf['Delta_n'],facecolors='none',s=10,edgecolors='k')
-#     plt.ylabel(r'$\delta_n$')
+    plt.scatter(mdf['F125W_Kron'],mdf['Delta_n'],facecolors='none',s=10,edgecolors='k')
+    plt.ylabel(r'$\delta_n$')
+    plt.xlabel(r'$F125W_{Kron}$')
+    plt.ylim([-2,2])
+    plt.show()
+    
+    plt.scatter(mdf['F125W_Kron'],mdf['Delta_re'],facecolors='none',s=10,edgecolors='k')
+    plt.ylabel(r'$\delta_{re}$')
+    plt.xlabel(r'$F125W_{Kron}$')
+    plt.ylim([-0.5,0.5])
+#     plt.yscale('log')
+    plt.show()
+    
+    plt.scatter(mdf['F125W_Kron'],mdf['Delta_q'],facecolors='none',s=10,edgecolors='k')
+    plt.ylabel(r'$\delta_q$')
+    plt.xlabel(r'$F125W_{Kron}$')
+    plt.ylim([-1,1])
+    plt.show()
+    
+    plt.scatter(mdf['F125W_Kron'],mdf['Delta_mag'],facecolors='none',s=10,edgecolors='k')
+    plt.ylabel(r'$\delta_{mag}$')
+    plt.xlabel(r'$F125W_{Kron}$')
+    plt.ylim([-1,1])
+    plt.show()
+    
+#     plt.scatter(mdf['F125W_Kron'],mdf['Delta_n']/mdf['n_C'],facecolors='none',s=10,edgecolors='k')
+#     plt.ylabel(r'$\delta_n/n_c$')
 #     plt.xlabel(r'$F125W_{Kron}$')
-#     plt.ylim([-2,2])
 #     plt.show()
     
-#     plt.scatter(mdf['F125W_Kron'],mdf['Delta_re'],facecolors='none',s=10,edgecolors='k')
-#     plt.ylabel(r'$\delta_{re}$')
-#     plt.xlabel(r'$F125W_{Kron}$')
-#     plt.ylim([-0.5,0.5])
-# #     plt.yscale('log')
-#     plt.show()
+    plt.scatter(mdf['n_M'],mdf['n_C'],facecolors='none',s=10,edgecolors='k')
+    plt.plot([0,8],[0,8],'k--')
+    plt.xlabel(r'Measured $n$')
+    plt.ylabel(r'Catalog $n$')
+    plt.xlim([0,8])
+    plt.ylim([0,8])
+    plt.show()
     
-#     plt.scatter(mdf['F125W_Kron'],mdf['Delta_q'],facecolors='none',s=10,edgecolors='k')
-#     plt.ylabel(r'$\delta_q$')
-#     plt.xlabel(r'$F125W_{Kron}$')
-#     plt.ylim([-1,1])
-#     plt.show()
-    
-#     plt.scatter(mdf['F125W_Kron'],mdf['Delta_mag'],facecolors='none',s=10,edgecolors='k')
-#     plt.ylabel(r'$\delta_{mag}$')
-#     plt.xlabel(r'$F125W_{Kron}$')
-#     plt.ylim([-1,1])
-#     plt.show()
-    
-# #     plt.scatter(mdf['F125W_Kron'],mdf['Delta_n']/mdf['n_C'],facecolors='none',s=10,edgecolors='k')
-# #     plt.ylabel(r'$\delta_n/n_c$')
-# #     plt.xlabel(r'$F125W_{Kron}$')
-# #     plt.show()
-    
-#     plt.scatter(mdf['n_M'],mdf['n_C'],facecolors='none',s=10,edgecolors='k')
-#     plt.plot([0,8],[0,8],'k--')
-#     plt.xlabel(r'Measured $n$')
-#     plt.ylabel(r'Catalog $n$')
-#     plt.xlim([0,8])
-#     plt.ylim([0,8])
-#     plt.show()
-    
-#     print "Mean abs dex difference between measured and catalog n:", np.mean(np.sqrt((mdf['logn_M']-mdf['logn_C'])**2))
-#     print "Median abs dex difference between measured and catalog n:", np.median(np.sqrt((mdf['logn_M']-mdf['logn_C'])**2))
-#     print "Mean abs diff between measured and catalog n:", np.mean(abs(mdf['n_M']-mdf['n_C']))
-#     print "Median abs diff between measured and catalog n:", np.median(abs(mdf['n_M']-mdf['n_C']))
-#     print "Mean difference between measured and catalog n:", np.mean(mdf['n_M']-mdf['n_C'])
-#     print "Median difference between measured and catalog n:", np.median(mdf['n_M']-mdf['n_C'])
-#     print "STD of difference between measured and catalog n:", np.std(mdf['n_M']-mdf['n_C'])
-#     print "Median offset in delta(n)/n in dex:", np.median((mdf['logn_M']-mdf['logn_C'])/mdf['logn_C'])
-#     print "Scatter in in delta(n)/n in dex:", np.std((mdf['logn_M']-mdf['logn_C'])/mdf['logn_C'])
+    print "Mean abs dex difference between measured and catalog n:", np.mean(np.sqrt((mdf['logn_M']-mdf['logn_C'])**2))
+    print "Median abs dex difference between measured and catalog n:", np.median(np.sqrt((mdf['logn_M']-mdf['logn_C'])**2))
+    print "Mean abs diff between measured and catalog n:", np.mean(abs(mdf['n_M']-mdf['n_C']))
+    print "Median abs diff between measured and catalog n:", np.median(abs(mdf['n_M']-mdf['n_C']))
+    print "Mean difference between measured and catalog n:", np.mean(mdf['n_M']-mdf['n_C'])
+    print "Median difference between measured and catalog n:", np.median(mdf['n_M']-mdf['n_C'])
+    print "STD of difference between measured and catalog n:", np.std(mdf['n_M']-mdf['n_C'])
+    print "Median offset in delta(n)/n in dex:", np.median((mdf['logn_M']-mdf['logn_C'])/mdf['logn_C'])
+    print "Scatter in in delta(n)/n in dex:", np.std((mdf['logn_M']-mdf['logn_C'])/mdf['logn_C'])
     
     
-#     mdf['logr_M'] = np.log10(mdf['re_M'])
-#     mdf['logr_C'] = np.log10(mdf['re_C'])
+    mdf['logr_M'] = np.log10(mdf['re_M'])
+    mdf['logr_C'] = np.log10(mdf['re_C'])
     
-#     plt.scatter(mdf['re_M'],mdf['re_C'],facecolors='none',s=10,edgecolors='k')
-#     plt.plot([0,8],[0,8],'k--')
-#     plt.xlabel(r'Measured $r_e$')
-#     plt.ylabel(r'Catalog $r_e$')
-#     plt.xlim([0,1])
-#     plt.ylim([0,1])
-#     plt.show()    
+    plt.scatter(mdf['re_M'],mdf['re_C'],facecolors='none',s=10,edgecolors='k')
+    plt.plot([0,8],[0,8],'k--')
+    plt.xlabel(r'Measured $r_e$')
+    plt.ylabel(r'Catalog $r_e$')
+    plt.xlim([0,1])
+    plt.ylim([0,1])
+    plt.show()    
     
-#     print "Mean offset between measured and cataloged re (percentage):", np.round(100*np.mean((mdf['re_M']-mdf['re_C'])/mdf['re_C']),decimals=2)
-#     print "Mean abs offset between measured and cataloged re (percentage):", np.round(100*np.mean(abs(mdf['re_M']-mdf['re_C'])/mdf['re_C']),decimals=2)
+    print "Mean offset between measured and cataloged re (percentage):", np.round(100*np.mean((mdf['re_M']-mdf['re_C'])/mdf['re_C']),decimals=2)
+    print "Mean abs offset between measured and cataloged re (percentage):", np.round(100*np.mean(abs(mdf['re_M']-mdf['re_C'])/mdf['re_C']),decimals=2)
     
-#     print "Median offset between measured and cataloged re (percentage):", np.round(100*np.median((mdf['re_M']-mdf['re_C'])/mdf['re_C']),decimals=2)
-#     print "Median abs offset between measured and cataloged re (percentage):", np.round(100*np.median(abs(mdf['re_M']-mdf['re_C'])/mdf['re_C']),decimals=2)
+    print "Median offset between measured and cataloged re (percentage):", np.round(100*np.median((mdf['re_M']-mdf['re_C'])/mdf['re_C']),decimals=2)
+    print "Median abs offset between measured and cataloged re (percentage):", np.round(100*np.median(abs(mdf['re_M']-mdf['re_C'])/mdf['re_C']),decimals=2)
  
-#     print "Mean abs dex difference between measured and catalog re:", np.mean(np.sqrt((mdf['logr_M']-mdf['logr_C'])**2))
-#     print "Median abs dex difference between measured and catalog re:", np.median(np.sqrt((mdf['logr_M']-mdf['logr_C'])**2))
-#     print "Mean abs diff between measured and catalog re:", np.mean(abs(mdf['re_M']-mdf['re_C']))
-#     print "Median abs diff between measured and catalog re:", np.median(abs(mdf['re_M']-mdf['re_C']))
-#     print "Mean difference between measured and catalog re:", np.mean(mdf['re_M']-mdf['re_C'])
-#     print "Median difference between measured and catalog re:", np.median(mdf['re_M']-mdf['re_C'])    
+    print "Mean abs dex difference between measured and catalog re:", np.mean(np.sqrt((mdf['logr_M']-mdf['logr_C'])**2))
+    print "Median abs dex difference between measured and catalog re:", np.median(np.sqrt((mdf['logr_M']-mdf['logr_C'])**2))
+    print "Mean abs diff between measured and catalog re:", np.mean(abs(mdf['re_M']-mdf['re_C']))
+    print "Median abs diff between measured and catalog re:", np.median(abs(mdf['re_M']-mdf['re_C']))
+    print "Mean difference between measured and catalog re:", np.mean(mdf['re_M']-mdf['re_C'])
+    print "Median difference between measured and catalog re:", np.median(mdf['re_M']-mdf['re_C'])    
         
-#     mdf['q_M'] = mdf['ar']
-#     mdf['q_C'] = mdf['q']
-#     mdf['logq_M'] = np.log10(mdf['q_M'])
-#     mdf['logq_C'] = np.log10(mdf['q_C'])
+    mdf['q_M'] = mdf['ar']
+    mdf['q_C'] = mdf['q']
+    mdf['logq_M'] = np.log10(mdf['q_M'])
+    mdf['logq_C'] = np.log10(mdf['q_C'])
     
-#     plt.scatter(mdf['q_M'],mdf['q_C'],facecolors='none',s=10,edgecolors='k')
-#     plt.plot([0,1],[0,1],'k--')
-#     plt.xlabel(r'Measured $q$')
-#     plt.ylabel(r'Catalog $q$')
-#     plt.xlim([0,1])
-#     plt.ylim([0,1])
-#     plt.show()    
+    plt.scatter(mdf['q_M'],mdf['q_C'],facecolors='none',s=10,edgecolors='k')
+    plt.plot([0,1],[0,1],'k--')
+    plt.xlabel(r'Measured $q$')
+    plt.ylabel(r'Catalog $q$')
+    plt.xlim([0,1])
+    plt.ylim([0,1])
+    plt.show()    
     
-#     print "Mean dex difference between measured and catalog q:", np.mean(np.sqrt((mdf['logq_M']-mdf['logq_C'])**2))
-#     print "Median dex difference between measured and catalog q:", np.median(np.sqrt((mdf['logq_M']-mdf['logq_C'])**2))
-#     print "Mean abs dex diff between measured and catalog n:", np.mean(abs(mdf['logq_M']-mdf['logq_C']))
-#     print "Median abs dex diff between measured and catalog n:", np.median(abs(mdf['logq_M']-mdf['logq_C']))
-#     print "Mean dex difference between measured and catalog q:", np.mean(mdf['logq_M']-mdf['logq_C'])
-#     print "Median dex difference between measured and catalog q:", np.median(mdf['logq_M']-mdf['logq_C'])
+    print "Mean dex difference between measured and catalog q:", np.mean(np.sqrt((mdf['logq_M']-mdf['logq_C'])**2))
+    print "Median dex difference between measured and catalog q:", np.median(np.sqrt((mdf['logq_M']-mdf['logq_C'])**2))
+    print "Mean abs dex diff between measured and catalog n:", np.mean(abs(mdf['logq_M']-mdf['logq_C']))
+    print "Median abs dex diff between measured and catalog n:", np.median(abs(mdf['logq_M']-mdf['logq_C']))
+    print "Mean dex difference between measured and catalog q:", np.mean(mdf['logq_M']-mdf['logq_C'])
+    print "Median dex difference between measured and catalog q:", np.median(mdf['logq_M']-mdf['logq_C'])
  
-#     plt.scatter(mdf['Delta_re'],mdf['Delta_n'],facecolors='none',s=10,edgecolors='k')
-#     plt.ylabel(r'$\delta_n$')
-#     plt.xlabel(r'$\delta_{re}$')
-#     plt.ylim([-2,2])
-#     plt.axvline(0,color='k',linestyle='--')
-#     plt.axhline(0,color='k',linestyle='--')
-#     plt.xlim([-0.3,0.3])
-#     plt.show()
+    plt.scatter(mdf['Delta_re'],mdf['Delta_n'],facecolors='none',s=10,edgecolors='k')
+    plt.ylabel(r'$\delta_n$')
+    plt.xlabel(r'$\delta_{re}$')
+    plt.ylim([-2,2])
+    plt.axvline(0,color='k',linestyle='--')
+    plt.axhline(0,color='k',linestyle='--')
+    plt.xlim([-0.3,0.3])
+    plt.show()
     
-#     plt.scatter(mdf['Delta_q'],mdf['Delta_n'],facecolors='none',s=10,edgecolors='k')
-#     plt.ylabel(r'$\delta_n$')
-#     plt.xlabel(r'$\delta_{q}$')
-#     plt.axvline(0,color='k',linestyle='--')
-#     plt.axhline(0,color='k',linestyle='--')
-#     plt.ylim([-2,2])
-#     plt.xlim([-0.3,0.3])
-#     plt.show()
+    plt.scatter(mdf['Delta_q'],mdf['Delta_n'],facecolors='none',s=10,edgecolors='k')
+    plt.ylabel(r'$\delta_n$')
+    plt.xlabel(r'$\delta_{q}$')
+    plt.axvline(0,color='k',linestyle='--')
+    plt.axhline(0,color='k',linestyle='--')
+    plt.ylim([-2,2])
+    plt.xlim([-0.3,0.3])
+    plt.show()
 
-#     if sky:
-# #         print mdf['sky'].describe()
-#         reds = plt.get_cmap("Reds")
-#         seismic = plt.get_cmap("seismic")
-#         plt.scatter(mdf['n_M'],mdf['n_C'],c=mdf['sky'],cmap=seismic,s=50,edgecolors='k',alpha=0.7,vmin=-1,vmax=1)
-#         plt.plot([0,8],[0,8],'k--')
-#         plt.xlabel(r'Measured $n$')
-#         plt.ylabel(r'Catalog $n$')
-#         plt.xlim([0,8])
-#         plt.ylim([0,8])
-#         cbar = plt.colorbar(orientation='vertical')
-#         cbar.set_label(r'Measured Sky level (counts)', fontsize=14)
-#         plt.show()
-#     return
+    if sky_test:
+#         print mdf['sky'].describe()
+        reds = plt.get_cmap("Reds")
+        seismic = plt.get_cmap("seismic")
+        plt.scatter(mdf['n_M'],mdf['n_C'],c=mdf['sky'],cmap=seismic,s=50,edgecolors='k',alpha=0.7,vmin=-1,vmax=1)
+        plt.plot([0,8],[0,8],'k--')
+        plt.xlabel(r'Measured $n$')
+        plt.ylabel(r'Catalog $n$')
+        plt.xlim([0,8])
+        plt.ylim([0,8])
+        cbar = plt.colorbar(orientation='vertical')
+        cbar.set_label(r'Measured Sky level (counts)', fontsize=14)
+        plt.show()
+    return
